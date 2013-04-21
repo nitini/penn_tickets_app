c26210d7d7458f1ec7be22c4c58dec7a7cfa6e9e
Greg Terrono <gterrono@seas.upenn.edu>
2013-04-21 16:34:46 -0400

    made some changes a few weeks ago

--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 from flask import session, request, g
 import bcrypt
 from ticket_app import db
+
 class UserError(Exception):
 
     def __init__(self, value):
@@ -30,3 +31,46 @@ class User(db.Model):
 
     def validate_password(self, password):
         return bcrypt.hashpw(password, self.hash) == self.hash
+
+
+#TODO: Make mixin
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    _user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    _user = db.relationship("User", backref=db.backref("student", uselist=False))
+
+    def __init__(self, email, password, confirm_password):
+        self._user = User(email, password, confirm_password)
+
+    @property
+    def email(self):
+        return self._user.email
+
+    def validate_password(self, password):
+        return self._user.validate_password(password)
+
+    @staticmethod
+    def student_with_email(email):
+        user = User.query.filter_by(email=email).first()
+        return user.student
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    _user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    _user = db.relationship("User", backref=db.backref("group", uselist=False))
+
+    def __init__(self, email, password, confirm_password):
+        self._user = User(email, password, confirm_password)
+
+    @property
+    def email(self):
+        return self._user.email
+
+    def validate_password(self, password):
+        return self._user.validate_password(password)
+
+    @staticmethod
+    def group_with_email(email):
+        user = User.query.filter_by(email=email).first()
+        return user.group
--- ticket_app.py
+++ ticket_app.py
@@ -11,19 +11,21 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    from models import User
-    user = None
-    if "user" in session:
-        user = User.query.get(session['user'])
-    return render_template('index.html', user=user)
+    from models import Student
+    student = None
+    if 'student' in session:
+        student = Student.query.get(session['student'])
+    return render_template('index.html', student=student)
 
 @app.route('/login', methods=['POST'])
 def login():
-    from models import User
+    from models import Student
     if request.form.get('email') and request.form.get('password'):
-        user = User.query.filter_by(email=request.form['email']).first()
-        if user.validate_password(request.form['password']):
-            session['user'] = str(user.id)
+        student = Student.student_with_email(request.form['email'])
+        if student.validate_password(request.form['password']):
+            if 'group' in session:
+                del session['group']
+            session['student'] = str(student.id)
             flash("You are Logged in!")
         else:
             flash("Email or Password Invalid")
@@ -34,10 +36,10 @@ def login():
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    from models import User, UserError
+    from models import Student, UserError
     try:
-        user = User(request.form.get('email'), request.form.get('password'), request.form.get('confirm_password'))
-        db.session.add(user)
+        student = Student(request.form.get('email'), request.form.get('password'), request.form.get('confirm_password'))
+        db.session.add(student)
         db.session.commit()
     except UserError as e:
         flash(str(e))
@@ -45,15 +47,51 @@ def signup():
 
 @app.route('/logout')
 def logout():
-    del session['user']
+    del session['student']
     flash("You are Logged Out!")
     return redirect("/")
 
+@app.route('/group')
+def group():
+    from models import Group
+    group = None
+    if 'group' in session:
+        group = Group.query.get(session['group'])
+    return render_template('group.html', group=group)
+
+@app.route('/group_login', methods=['POST'])
+def group_login():
+    from models import Group
+    if request.form.get('email') and request.form.get('password'):
+        group = Group.group_with_email(request.form['email'])
+        if group.validate_password(request.form['password']):
+            if 'student' in session:
+                del session['student']
+            session['group'] = str(group.id)
+            flash("You are Logged in!")
+        else:
+            flash("Email or Password Invalid")
+    else:
+        flash("Email and Password are Required")
+    return redirect("/group")
 
 
+@app.route('/group_signup', methods=['POST'])
+def group_signup():
+    from models import Group, UserError
+    try:
+        group = Group(request.form.get('email'), request.form.get('password'), request.form.get('confirm_password'))
+        db.session.add(group)
+        db.session.commit()
+    except UserError as e:
+        flash(str(e))
+    return redirect("/group")
+
+@app.route('/group_logout')
+def group_logout():
+    del session['group']
+    flash("You are Logged Out!")
+    return redirect("/group")
 
 if __name__=="__main__":
     app.run(debug=True)
-    
-
-
