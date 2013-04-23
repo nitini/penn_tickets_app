96ca54fa3128e1f0f6aad7f46e173b47a67efc0f
Greg Terrono <gterrono@seas.upenn.edu>
2013-04-23 12:50:35 -0400

    Got dropdown forms to disappear when the user clicks away

--- static/application.js
+++ static/application.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
+  var loginShouldBeShowing = false;
+  var signupShouldBeShowing = false;
   $('#login-dropdown').click(function() {
+    loginShouldBeShowing = true;
     $('#login-form-dropdown').toggle();
     $('#signup-form-dropdown').hide();
   });
   $('#signup-dropdown').click(function() {
+    signupShouldBeShowing = true;
     $('#signup-form-dropdown').toggle();
     $('#login-form-dropdown').hide();
   });
@@ -12,5 +16,19 @@ $(document).ready(function() {
     minuteStep: 1,
     showInputs: false,
     disableFocus: true
+  });
+  $('body').click(function() {
+    if(!loginShouldBeShowing)
+      $('#login-form-dropdown').hide();
+    if(!signupShouldBeShowing)
+      $('#signup-form-dropdown').hide();
+    loginShouldBeShowing = false;
+    signupShouldBeShowing = false;
+  });
+  $('#login-form-dropdown').click(function() {
+    loginShouldBeShowing = true;
+  });
+  $('#signup-form-dropdown').click(function() {
+    signupShouldBeShowing = true;
   });
 });
