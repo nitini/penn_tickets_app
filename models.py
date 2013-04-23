c151769e61d5076f41782d281658c8e4757d9e82
nitin.iyer.1103@gmail.com <nitin.iyer.1103@gmail.com>
2013-04-23 09:48:23 -0400

    Changes to View

--- models.py
+++ models.py
@@ -24,12 +24,13 @@ class User(app.db.Model):
     email = app.db.Column(app.db.String(120), unique=True)
     hash = app.db.Column(app.db.String())
 
-    def __init__(self, email, password, confirm_password):
-        if not email:
-            raise UserError("No Username given")
+    def __init__(self, name, email, password, confirm_password):
+        if not email or not name:
+            raise UserError("No Name or Email given")
         if password:
             if password == confirm_password:
                 hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+                self.name = name
                 self.email = email
                 self.hash = hashed_password
             else:
@@ -42,12 +43,16 @@ class User(app.db.Model):
 
 
 class TicketUser():
-    def __init__(self, email, password, confirm_password):
-        self._user = User(email, password, confirm_password)
+    def __init__(self, name, email, password, confirm_password):
+        self._user = User(name, email, password, confirm_password)
 
     @property
     def email(self):
         return self._user.email
+
+    @property
+    def name(self):
+        return self._user.name
 
     def validate_password(self, password):
         return self._user.validate_password(password)
@@ -61,12 +66,10 @@ class Student(TicketUser, app.db.Model):
     _user_id = app.db.Column(app.db.Integer, app.db.ForeignKey('user.id'))
     _user = app.db.relationship("User", backref=app.db.backref("student",
                                                                uselist=False))
-
     @staticmethod
     def get_by_email(email):
         user = User.query.filter_by(email=email).first()
         return user.student
-
 
 class Group(TicketUser, app.db.Model):
     id = app.db.Column(app.db.Integer, primary_key=True)
@@ -90,20 +93,25 @@ attendances = app.db.Table('attendances',
 class Event(app.db.Model):
     id = app.db.Column(app.db.Integer, primary_key=True)
     name = app.db.Column(app.db.String(80))
-    date = app.db.DateTime()
+    date = app.db.Column(app.db.DateTime(timezone=False))
     group_id = app.db.Column(app.db.Integer, app.db.ForeignKey('group.id'))
     description = app.db.Column(app.db.String(500))
+    max_attendees = app.db.Column(app.db.Integer())
+    ticket_price = app.db.Column(app.db.String(80))
+
     attendees = app.db.relationship('Student', secondary=attendances,
                                     lazy='dynamic',
                                     backref=app.db.backref('events',
                                                            lazy='dynamic'))
 
-    def __init__(self, name, description, date,  group, max_attendees):
+    def __init__(self, name, description, date,  group, max_attendees,
+                 ticket_price):
         self.name = name
         self.group = group
         self.date = date
         self.description = description
         self.max_attendees = max_attendees
+        self.ticket_price = ticket_price
         self.count = 0
 
     def __repr__(self):
