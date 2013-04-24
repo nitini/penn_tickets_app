e3e65d158471f83a98f3bf3be87160955ae388c8
Greg Terrono <gterrono@seas.upenn.edu>
2013-04-23 23:49:13 -0400

    A few last minute changes

--- ticket_app.py
+++ ticket_app.py
@@ -15,15 +15,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
                                                        'sqlite:///db.db')
 db = SQLAlchemy(app)
 
+
 def get_model(user_models, user_string):
     if (user_string in session):
         return user_models.query.get(session[user_string])
     else:
         return None
 
+
 def index(user_models, user_string, template, events=None):
     user = get_model(user_models, user_string)
     return render_template(template, user=user, events=events)
+
 
 def login(user_models, user_string, next_url='/'):
     if request.form.get('email') and request.form.get('password'):
@@ -40,6 +43,7 @@ def login(user_models, user_string, next_url='/'):
         flash("Email and Password are Required")
     return redirect(next_url)
 
+
 def signup(user_models, user_string, next_url='/'):
     try:
         user = user_models(request.form.get('name'),
@@ -54,9 +58,11 @@ def signup(user_models, user_string, next_url='/'):
         flash(str(e))
     return redirect(next_url)
 
+
 def logout(user_string, next_url='/'):
     del session[user_string]
     return redirect(next_url)
+
 
 @app.route("/")
 def student_index():
@@ -66,25 +72,31 @@ def student_index():
     return index(models.Student, 'student', 'index.html',
                  models.Event.query.all())
 
+
 @app.route('/login', methods=['POST'])
 def student_login():
     return login(models.Student, 'student')
+
 
 @app.route('/signup', methods=['POST'])
 def student_signup():
     return signup(models.Student, 'student')
 
+
 @app.route('/logout')
 def student_logout():
     return logout('student')
+
 
 @app.route('/group')
 def group():
     return index(models.Group, 'group', 'group.html')
 
+
 @app.route('/group_login', methods=['POST'])
 def group_login():
     return login(models.Group, 'group', '/group')
+
 
 @app.route('/group_signup', methods=['POST'])
 def group_signup():
@@ -95,13 +107,15 @@ def group_signup():
 def group_logout():
     return logout('group', '/group')
 
+
 @app.route('/create_event', methods=['POST'])
 def create_event():
     group = get_model(models.Group, 'group')
     frmt = "%m/%d/%y %I:%M %p"
     if group:
         try:
-            date_time = request.form.get('date') + " " + request.form.get('time')
+            date_time = (request.form.get('date') + " " +
+                         request.form.get('time'))
             event = models.Event(request.form.get('name'),
                                  request.form.get('description'),
                                  datetime.strptime(date_time, frmt), group,
@@ -116,6 +130,7 @@ def create_event():
         flash("Must Be Logged In to Create an Event")
     return redirect('/group')
 
+
 @app.route("/event/<event_id>", methods=['GET'])
 def view_event(event_id):
     event = models.Event.query.get(event_id)
@@ -125,26 +140,33 @@ def view_event(event_id):
     if group is not None:
         return render_template('group_view_event.html', event=event,
                                user=group, attendees=attendees)
-    else: 
+    else:
         return render_template('view_event.html', event=event, user=user)
+
 
 @app.route('/event_delete/<event_id>', methods=['POST'])
 def delete_event(event_id):
     event_del = models.Event.query.get(event_id)
     if event_del:
-        event_del = models.Event.query.get(event_id)
-        db.session.expunge(event_del)
-        db.session.commit()
-        db.session.flush()
+        group = get_model(models.Group, 'group')
+        if group:
+            if event_del.group == group:
+                models.Event.query.session.delete(event_del)
+                models.Event.query.session.commit()
+                models.Event.query.session.flush()
+                return redirect('/group')
+        flash("You do not have permission to delete this event")
+        return redirect('/event/' + event_id)
     else:
         flash("Sorry you cannot delete the event at this time")
-    return redirect('/event/'+ event_id)
+        return redirect('/event/' + event_id)
 
 
 @app.route("/group_event/<event_id>", methods=['GET'])
 def group_view_event(event_id):
     event = models.Event.query.get(event_id)
     return render_template('group_view_event.html', event=event)
+
 
 @app.route("/event/<event_id>/purchase", methods=['POST'])
 def attending_event(event_id):
@@ -158,10 +180,12 @@ def attending_event(event_id):
         flash("You must be logged in to purchase a ticket")
     return redirect("/event/" + event_id)
 
+
 @app.route("/group/<event_id>/printable_attendee_list")
 def get_attendee_list(event_id):
     event = models.Event.query.get(event_id)
     return render_template('print_attendees.html', event=event)
+
 
 @app.route('/leaderboard')
 def leader_board():
@@ -174,9 +198,11 @@ def leader_board():
             happening.append(event)
     return render_template('leaderboard.html', leaders=this_week)
 
+
 def datetime_to_str(date):
     frmt = "%I:%M %p, %m/%d/%y"
     return date.strftime(frmt)
+
 
 def sort_events(events):
     return sorted(events, key=lambda e: e.date)
